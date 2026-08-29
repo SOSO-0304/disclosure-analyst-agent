d287edd772984badc17347f8007bcaaad9dfd5ba
Sohyun Jee <77823902+SOSO-0304@users.noreply.github.com>
2026-08-29 14:19:49 +0900

    chore: export effective canonical reader

--- src/disclosure_agent/storage/__init__.py
+++ src/disclosure_agent/storage/__init__.py
@@ -3,7 +3,13 @@
 from disclosure_agent.storage.jsonl import (
     append_canonical,
     read_canonical,
+    read_effective_canonical,
     write_canonical,
 )
 
-__all__ = ["append_canonical", "read_canonical", "write_canonical"]
+__all__ = [
+    "append_canonical",
+    "read_canonical",
+    "read_effective_canonical",
+    "write_canonical",
+]

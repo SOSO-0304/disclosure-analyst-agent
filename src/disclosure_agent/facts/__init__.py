88bd1afdd362834ac4633837dae310a52f1da8a1
Sohyun Jee <77823902+SOSO-0304@users.noreply.github.com>
2026-08-29 21:56:24 +0900

    feat: add generic fact extraction package

---
+++ src/disclosure_agent/facts/__init__.py
@@ -0,0 +1,15 @@
+"""Generic atomic facts derived conservatively from canonical tables."""
+
+from disclosure_agent.facts.generic import (
+    FactKind,
+    GenericFact,
+    extract_generic_facts,
+    extract_table_facts,
+)
+
+__all__ = [
+    "FactKind",
+    "GenericFact",
+    "extract_generic_facts",
+    "extract_table_facts",
+]
